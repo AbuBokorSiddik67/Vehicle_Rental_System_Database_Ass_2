606d01320eb9f351dfebdf194681c44bd7ef105d
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2025-12-23 18:47:06 +0600

    complete project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Vehicle_Rental_System_Database_Ass_2
+# Vehicle Rental System Database Design And Some Query Questions & Answers With Postgres Sql.
+
+First we will create a database with CREATE DATABASE vehicle rental databse this command.
+Then we follow same procedure and create three table that's Users, Vehicles and Bookings.
+We will run our all code is vs code. Then read the problem and first will will make a ER diagram in any tools. So we use lucid.app for making this ER diagram. This diagram link is bellow.
+
+ERD Link: https://lucid.app/lucidchart/02f6e6a3-d6fd-41e4-a3c2-52437d46799e/edit?viewport_loc=-3144%2C-750%2C2298%2C1038%2C0_0&invitationId=inv_6c35a526-8e70-425a-a84f-82f7cd9e480c
+
+Now will solve this problem.
+
+Question_1: Retrieve booking information along with: Customer name, Vehicle name.
+Ans:
+If we want solve this problem we are use 'Inner Join'. First we join Users and Vehicles table and show all requered data like User role (Admin or Customer), Name, email, password, phone number Each email must be unique (no duplicate accounts).
+
+Question_2: Find all vehicles that have never been booked.
+Ans:
+This problem is very essay. Just we use a simple logic like which vehicles status is 'rented' we just ignore one and show all vehicles. Command is: select * from "Vehicles" as v where v.status <> 'rented';
+
+Question_3: Retrieve all available vehicles of a specific type (e.g. cars).
+Ans:
+Here we will use a function. The function get one paramitter and this is vehicles type. We just make a function and write a simple query such as select, where etc.
+
+Question_4: Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+Ans:
+This question is hard but very simple. just we will used 'group by and having' and join 'Booking and Vehicles' tables. There show only booking vehicles name and how much booking in this single vehicles.
--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
+--? Table Creation...
 CREATE TYPE vehicle_status AS ENUM ('available','rented','maintenance');
 
-CREATE TYPE booking_status AS ENUM('pending','confirmed','completed');
+CREATE TYPE booking_status AS ENUM('pending','confirmed','completed','cancelled');
 
 CREATE TYPE vehicle_type AS ENUM('car','bike','truck');
 
@@ -10,12 +11,13 @@ CREATE TABLE "Users" (
     "name" varchar(50),
     "email" varchar(50) unique,
     "password" varchar(50),
-    "phone_number" varchar(50),
+    "phone" varchar(50),
     PRIMARY KEY ("user_id")
 );
 
 CREATE TABLE "Vehicles" (
     "vehicle_id" bigint,
+    "name" varchar(100),
     "type" vehicle_type,
     "model" varchar(50),
     "registration_number" varchar(100) unique,
@@ -37,112 +39,65 @@ CREATE TABLE "Bookings" (
     CONSTRAINT "FK_Bookings_vehicle_id" FOREIGN KEY ("vehicle_id") REFERENCES "Vehicles" ("vehicle_id")
 );
 
-SELECT * FROM "Users";
+--? Users Data
+SELECT
+    user_id,
+    name,
+    email,
+    phone_number,
+    role
+FROM "Users";
 
+--? Vehicles Data
 SELECT * FROM "Vehicles";
 
+--? Bookings Data
 SELECT * FROM "Bookings";
 
-INSERT INTO
-    "Users" (
-        user_id,
-        role,
-        name,
-        email,
-        password,
-        phone_number
-    )
-VALUES (
-        1,
-        'customer',
-        'Arif Hossain',
-        'arif@email.com',
-        'pass123',
-        '01711223344'
-    ),
-    (
-        2,
-        'customer',
-        'Nusrat Jahan',
-        'nusrat@email.com',
-        'secure456',
-        '01822334455'
-    );
-
-INSERT INTO
-    "Vehicles" (
-        vehicle_id,
-        type,
-        model,
-        registration_number,
-        rental_price,
-        status
-    )
-VALUES (
-        101,
-        'car',
-        'Toyota Corolla',
-        'DHAKA-METRO-123',
-        3000,
-        'available'
-    ),
-    (
-        102,
-        'bike',
-        'Yamaha R15',
-        'DHAKA-METRO-456',
-        1200,
-        'available'
-    ),
-    (
-        103,
-        'truck',
-        'Tata LPT',
-        'DHAKA-METRO-789',
-        8000,
-        'maintenance'
-    );
-
-INSERT INTO
-    "Bookings" (
-        booking_id,
-        user_id,
-        vehicle_id,
-        start_date,
-        end_date,
-        status,
-        total_cost
-    )
-VALUES (
-        501,
-        1,
-        101,
-        '2025-01-01',
-        '2025-01-03',
-        'confirmed',
-        6000
-    ),
-    (
-        502,
-        2,
-        102,
-        '2025-01-05',
-        '2025-01-05',
-        'pending',
-        1200
-    );
-
---? Retrieve booking information along with Customer name and Vehicle name.
+--?-1 Retrieve booking information along with Customer name and Vehicle name.
 SELECT
     b.booking_id,
     u.name AS customer_name,
-    v.model AS vehicle_name,
+    v.name AS vehicle_name,
     b.start_date,
     b.end_date,
     b.status
 FROM
     "Bookings" AS b
-    JOIN "Users" AS u USING (user_id)
-    JOIN "Vehicles" AS v USING (vehicle_id);
+    INNER JOIN "Users" AS u USING (user_id)
+    INNER JOIN "Vehicles" AS v USING (vehicle_id);
 
---? Find all vehicles that have never been booked.
+--?-2 Find all vehicles that have never been booked.
+SELECT *
+FROM "Vehicles" as v
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM "Vehicles" as v2
+        WHERE
+            v.vehicle_id = v2.vehicle_id
+            AND v.status = 'rented'
+    );
+
+--?-3 Retrieve all available vehicles of a specific type (e.g. cars).
+CREATE OR REPLACE FUNCTION search_vehicle_type(type_search vehicle_type)
+RETURNS SETOF "Vehicles" AS
+$$
+BEGIN
+RETURN QUERY
+SELECT * FROM "Vehicles" AS v WHERE v.type=type_search;
+END;
+$$ LANGUAGE PLPGSQL;
+
+SELECT * FROM search_vehicle_type ('bike');
+
+--?-4 Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+SELECT
+    v.name as vehicle_name,
+    COUNT(*) as total_bookings
+FROM "Bookings" as b
+    INNER JOIN "Vehicles" as v USING (vehicle_id)
+GROUP BY
+    v.vehicle_id
+HAVING
+    COUNT(*) > 2;
